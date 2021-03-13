67fd68078470eea88fc4d4f43783c11543ddb4be
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-13 17:24:23 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -9,7 +9,12 @@
 int _printf(const char *format, ...)
 {
 	char tag, *check, temp;
-	int count = 0;
+	int count = 0, i = 0, flag = 0;
+	void (*f)(va_list args, int *count);
+	map mapping[] = {
+		{'c', print_char},{'d', print_int},
+		{'i', print_int},{'s', print_str}
+	}
 	va_list args;
 
 	va_start(args, format);
@@ -18,28 +23,19 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			switch (*format)
+			for (; i < 5; i++)
 			{
-				case 'c':
-					temp = va_arg(args, int);
-					write(1, &temp, 1);
-					format++;
+				if (*format == mapping[i].type)
+				{
+					f = mappping[i].func;
+					f(args, &count);
+					flag = 1;
 					break;
-// 				case 'd':
-// 					print(va_arg(args, int), &count);
-// 					format++;
-// 					break;
-				case '%':
-					print("%", &count);
-					format++;
-					break;;
-				case 's':
-					print(va_arg(args, char *), &count);
-					format++;
-					break;
-				default:
-					format++;
-					break;
+				}
+			}
+			if (!flag)
+			{
+				
 			}
 		}
 		else
@@ -49,7 +45,30 @@ int _printf(const char *format, ...)
 			format++;
 		}
 	}
-	printf("\n");
 	va_end(args);
 	return (count);
 }
+// 			switch (*format)
+// 			{
+// 				case 'c':
+// 					temp = va_arg(args, int);
+// 					write(1, &temp, 1);
+// 					format++;
+// 					break;
+// // 				case 'd':
+// // 					print(va_arg(args, int), &count);
+// // 					format++;
+// // 					break;
+// 				case '%':
+// 					print("%", &count);
+// 					format++;
+// 					break;;
+// 				case 's':
+// 					print(va_arg(args, char *), &count);
+// 					format++;
+// 					break;
+// 				default:
+// 					format++;
+// 					break;
+// 			}
+		}
