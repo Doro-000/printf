03ec1987b487d5c52ed49486f2d473eaf2254e11
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-15 21:21:47 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if ((format == NULL) || (*format == '%' && *(format + 1) == '\0'))
+	if (format == NULL)
 		return (-1);
 	while ((*format) != '\0')
 	{
