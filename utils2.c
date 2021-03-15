0704cfee100cc371a754cab913ca2d3dbd93872c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-15 13:50:39 +0300

    Update utils2.c

--- utils2.c
+++ utils2.c
@@ -1,4 +1,11 @@
 #include "holberton.h"
+/**
+ * print_bin - prints an bin to stdout;
+ * @args: variable arguments passed to _printf
+ * @count: int to be used by print, see description for print in helpers.c
+ *
+ * Return: void
+ */
 
 void print_bin(va_list args, int *count)
 {
@@ -6,10 +13,28 @@ void print_bin(va_list args, int *count)
 	int num = va_arg(args, int);
 	int i = 0;
 
-	bin_string = malloc(sizeof(char) * (binlen(num) + 1));
+	bin_string = malloc(sizeof(char) * (_numlen(num) + 1));
 	if (bin_string != NULL)
 	{
 		_itoa(num, bin_string);
 	}
+	print(bin_string, count);
+}
+/**
+ * print_S - prints the string to stdout;
+ * @args: variable arguments passed to _printf
+ * @count: int to be used by print, see description for print in helpers.c
+ *
+ * Return: void
+ */
+void print_S(va_list args, int *count)
+{
+	char *string;
+	int i = 0;
+
+// 	for (; i < 
+// 	{
+// 		_itoa(num, bin_string);
+// 	}
 	print(bin_string, count);
 }
