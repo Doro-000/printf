88fc84ba55dafb0c83375ea32ae84c0e50cf7b7e
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-15 13:27:58 +0300

    Update utils2.c

--- utils2.c
+++ utils2.c
@@ -1,1 +1,15 @@
 #include "holberton.h"
+
+void print_bin(va_list args, int *count)
+{
+	char *bin_string;
+	int num = va_arg(args, int);
+	int i = 0;
+
+	bin_string = malloc(sizeof(char) * (binlen(num) + 1));
+	if (bin_string != NULL)
+	{
+		_itoa(num, bin_string);
+	}
+	print(bin_string, count);
+}
