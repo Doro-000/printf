f4e095773a912d6b69050d56f0833cf361204636
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-15 17:38:00 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if ((format == NULL) || (*format == '%' && *(format + 1) == '\0'))
 		return (-1);
-	while ((*format) != '\0')
+	while ((*format) != '\0' && (*(format + 1) != '\0'))
 	{
 		if (((*format) == '%') && (*(format + 1) != '\0'))
 		{
