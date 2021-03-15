d5f39be7720b19b4bcbc7b6e671d34c677a92dab
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-15 16:56:59 +0300

    Update test.c

--- test.c
+++ test.c
@@ -11,19 +11,11 @@ int main(void)
 {
     int len;
     int len2;
-<<<<<<< HEAD
     unsigned int ui;
-//     void *addr;
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
-//     addr = (void *)0x7ffe637541f0;
-=======
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
->>>>>>> 6b85e694cc0c0df61ac4980b9ede6d856a9901ee
     printf("Length:[%d, %d]\n", len, len);
     printf("Length:[%d, %d]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
