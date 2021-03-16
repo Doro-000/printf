40fc925e67578083e5e08f3b12c053e5006488ee
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-16 14:54:37 +0300

    Update utils2.c

--- utils2.c
+++ utils2.c
@@ -63,6 +63,7 @@ void print_S(va_list args, int *count)
 		for (; string[i] != '\0'; i++)
 		{
 			if ((string[i] > 0 && string[i] < 32) || (string[i] >= 127))
+			{
 				hex = malloc(sizeof(char) * (_numlen(string[i], 16)));
 				if (_numlen(string[i], 16) == 2)
 					print("\\x", count);
@@ -78,7 +79,7 @@ void print_S(va_list args, int *count)
 				}
 				print(hex, count);
 				free(hex);
-				
+			}
 			else
 			{
 				_putchar(string[i], count);
