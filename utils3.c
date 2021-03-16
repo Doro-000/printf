ef0e5ed98dc8a88f31bb44a2201758282972ba97
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-16 18:46:14 +0300

    Update utils3.c

--- utils3.c
+++ utils3.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
- * rot13 - rot13 encoder
- * @str: string to be encoded
+ * print_rot13 - encodes a string to rot13 and prints it
+ * @args: variable arguments passed to _printf
+ * @count: int to be used by _putchar, see _putchar in helpers.c
  *
- * Return: address of the encoded string
+ * Return: void
  */
 void print_rot13(va_list args, int *count)
 {
