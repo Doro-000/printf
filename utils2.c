5e94cdfe4a9fb9910dca1580a2154d8cd9bc535b
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-16 18:49:10 +0300

    Update utils2.c

--- utils2.c
+++ utils2.c
@@ -8,20 +8,19 @@
  */
 char *convert(unsigned int num, int base)
 {
-	static char Representation[]= "0123456789ABCDEF";
-	static char buffer[50];
+	char Representation[] = "0123456789ABCDEF";
+	char buffer[50];
 	char *ptr;
 
 	ptr = &buffer[49];
 	*ptr = '\0';
 
-	do
-	{
+	do{
 		*--ptr = Representation[num % base];
 		num /= base;
-	}while(num != 0);
+	} while (num != 0);
 
-	return(ptr);
+	return (ptr);
 }
 
 void print_bin(va_list args, int *count)
