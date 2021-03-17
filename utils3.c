36cd0b4a0d1ad398dda21b95a3f59ea6572ff59f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-17 16:13:17 +0300

    Update utils3.c

--- utils3.c
+++ utils3.c
@@ -24,3 +24,29 @@ void print_rot13(va_list args, int *count)
 	else
 		print("(null)", count);
 }
+
+/**
+ * print_unsigned - unsigned int and prints it
+ * @args: variable arguments passed to _printf
+ * @count: int to be used by _putchar, see _putchar in helpers.c
+ *
+ * Return: void
+ */
+void print_unsigned(va_list args, int *count)
+{
+	int i;
+	char *unsigned_string;
+	int num = va_arg(args, int);
+	unsigned int n;
+
+	n = num;
+	unsigned_string = malloc(sizeof(char) * (_numlen(n, 10) + 1));
+	if (unsigned_string != NULL)
+	{
+		_itoux((UINT_MAX + n), unsigned_string, 10);
+		print(unsigned_string, count);
+	}
+	else
+		exit(-1);
+	free(unsigned_string);
+}
